7e38d77b94e6afb5ae5d10fdf73429baf099f591
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-12-29 13:37:04 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 import subprocess
 import sys
 
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    for line in process.stdout:
+        print(line.strip())
+    for line in process.stderr:
+        print(line.strip(), file=sys.stderr)
+
+    process.wait()
+    return process.returncode 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    return_code = run_command([command] + args)
+    sys.exit(return_code)
 
 
 if __name__ == "__main__":
