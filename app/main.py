51800f31d006daf35b790907b9da11773d7ecbda
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-12-30 11:37:09 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,21 +1,45 @@
 import subprocess
 import sys
+import os
+import shutil
+import tempfile
 
-def run_command(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    for line in process.stdout:
-        print(line.strip())
-    for line in process.stderr:
-        print(line.strip(), file=sys.stderr)
+def setup_chroot_environment(command, args):
+    """
+    Sets up a chroot environment in a temporary directory and executes a command.
+    Args:
+        executable_path (str): Path to the binary to be executed.
+        command (list): Command to execute inside the chroot environment.
+    """
+    # Create a temporary directory for the chroot environment
+    chroot_dir = tempfile.mkdtemp()
+    try:
 
-    process.wait()
-    return process.returncode 
+        shutil.copy(command, chroot_dir)
+        os.chroot(chroot_dir)
+
+        # Execute the command inside the chroot jail
+        command = os.path.join("/", os.path.basename(command))
+        result = subprocess.run([command, *args], capture_output=True)
+
+        # Pipe the outputs (stdout and stderr) to the parent process
+        sys.stdout.write(result.stdout.decode("utf-8"))
+        sys.stderr.write(result.stderr.decode("utf-8"))
+
+        return_code = result.returncode
+        return return_code
+
+    finally:
+        # Clean up the temporary directory
+        if os.path.exists(chroot_dir):
+            shutil.rmtree(chroot_dir)
+
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    return_code = run_command([command] + args)
+    return_code = setup_chroot_environment(command, args)
     sys.exit(return_code)
 
 
